9482ba6c7ec34141dfe7e63d477f38f8aeea2654
Renan Simões Gonçalves <renansimoes200406@gmail.com>
2024-03-31 22:50:06 -0300

    Realização do segundo exercício pelo aluno Gabriel Guilherme (https://github.com/Manerrando28)

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 //EXERCÍCIO 1
 
 //VARIÁVEL COM VALOR UNDEFINED
-let var1 = undefined;
-console.log(var1);
+//let var1 = undefined;
+//console.log(var1);
 
 //VARIÁVEL COM VALOR VAZIO
-let var2 = null;
-console.log(var2);
+//let var2 = null;
+//console.log(var2);
 
 //VARIÁVEL COM VALOR
-let var3 = 100;
-console.log(var3);
+//let var3 = 100;
+//console.log(var3);
+
+//EXERCÍCIO 2
+
+let num1 = 100;
+let num2 = 5;
+
+//USANDO O OPERADOR !=
+console.log(num1 != num2);
+
+//USANDO O OPERADOR ==
+console.log(num1 == num2);
+
+//USANDO O OPERADOR >=
+console.log(num1 >= num2);
