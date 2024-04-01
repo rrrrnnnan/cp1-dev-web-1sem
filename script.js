48a17df98e3cba30a8893d74dcef358b4c81afbe
Renan Simões Gonçalves <renansimoes200406@gmail.com>
2024-03-31 23:09:45 -0300

    Realização do terceiro exercício pelo aluno Felipe Genistretti (https://github.com/FelipeGenistretti)

--- script.js
+++ script.js
@@ -14,14 +14,29 @@
 
 //EXERCÍCIO 2
 
-let num1 = 100;
-let num2 = 5;
+//let num1 = 100;
+//let num2 = 5;
 
 //USANDO O OPERADOR !=
-console.log(num1 != num2);
+//console.log(num1 != num2);
 
 //USANDO O OPERADOR ==
-console.log(num1 == num2);
+//console.log(num1 == num2);
 
 //USANDO O OPERADOR >=
-console.log(num1 >= num2);
+//console.log(num1 >= num2);
+
+//EXERCÍCIO 3
+ 
+let peso = parseFloat(prompt('Digite seu peso em quilogramas.'));
+let altura = parseFloat(prompt('Digite sua altura em metros.'));
+
+let IMC = peso / (altura * altura);
+   
+if (IMC < 18.5) {
+    console.log('Você está abaixo do peso.');
+} else if (IMC >= 18.5 && IMC <= 24.9) {
+    console.log('Você está no peso ideal.');
+} else {
+    console.log('Você está acima do peso.');
+}
