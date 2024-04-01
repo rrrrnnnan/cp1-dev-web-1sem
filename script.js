4c8242be564e563ebff00e272dfc073c49e28d5c
Renan Simões Gonçalves <renansimoes200406@gmail.com>
2024-03-31 22:42:25 -0300

    Realização do primeiro exercício pelo aluno Gabriel Guilherme (https://github.com/Manerrando28)

---
+++ script.js
@@ -0,0 +1,13 @@
+//EXERCÍCIO 1
+
+//VARIÁVEL COM VALOR UNDEFINED
+let var1 = undefined;
+console.log(var1);
+
+//VARIÁVEL COM VALOR VAZIO
+let var2 = null;
+console.log(var2);
+
+//VARIÁVEL COM VALOR
+let var3 = 100;
+console.log(var3);
