776faff8ca4137819f901ebbc52ef5ebc99df336
Renan Simões Gonçalves <renansimoes200406@gmail.com>
2024-04-01 21:04:29 -0300

    Realização do quinto exercício pelo aluno Felipe Genistretti (https://github.com/FelipeGenistretti)

--- script.js
+++ script.js
@@ -43,16 +43,28 @@
 
 //EXERCÍCIO 4
 
-let idade = parseInt(prompt("Qual a sua idade? "));
+//let idade = parseInt(prompt("Qual a sua idade? "));
 
-if (idade <= 12) {
-    console.log("Você é uma criança.");
-} else if (idade >= 13 && idade <= 18) {
-    console.log("Você é um adolescente.");
-} else if (idade >= 19 && idade <= 60) {
-    console.log("Você é um adulto.");
+//if (idade <= 12) {
+    //console.log("Você é uma criança.");
+//} else if (idade >= 13 && idade <= 18) {
+    //console.log("Você é um adolescente.");
+//} else if (idade >= 19 && idade <= 60) {
+    //console.log("Você é um adulto.");
+//} else {
+    //console.log("Você é um idoso.");
+//}
+
+//EXERCÍCIO 5
+
+let numeDeUsuario = "admin";
+let senha = 1234;
+
+let insiraNome = prompt('Insira o nome de usuário.');
+let insiraSenha = prompt('Insira a senha.');
+
+if (senha == insiraSenha && numeDeUsuario == insiraNome) {
+    console.log('Login efetuado com êxito.');
 } else {
-    console.log("Você é um idoso.");
+    console.log('Falha de autentificação.');
 }
-
-
